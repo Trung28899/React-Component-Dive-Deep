0b110e5713dcab50ec7feece7e51cfb2e4f767ef
Trung Trinh <you@example.com>
2020-05-18 17:55:04 -0400

    test commit

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -20,7 +20,7 @@ const cockpit = (props) => {
   return (
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
-      <p className={assignedClasses.join(" ")}>This is really working!</p>
+      <p className={assignedClasses.join(" ")}>This is really working !</p>
       <button className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
